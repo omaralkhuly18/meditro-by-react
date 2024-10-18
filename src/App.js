8b0e7f273719a7189b69e0054694f4647f3f94e3
omaralkhuly18 <omaralkhuly18@users.noreply.github.com>
2024-10-17 22:52:41 -0700

    commitA five

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React, { Fragment } from 'react';
 import './index.css';
 import ContainerNav from './components/Nav/Navbar';
-import ContainerHome from "./pages/Home"
-
+import ContainerHome from "./pages/Home";
+import ContainerFooter from './components/Footer/Footer';
 function App() {
   return (
     <Fragment>
       <ContainerNav/>
       <ContainerHome/>
+      <ContainerFooter/>
     </Fragment>
   );
 }
