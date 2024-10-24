33d1eef6117144c753514d67e13763b0c8ef1d05
omarelkhuly <silvertriangle11@gmail.com>
2024-10-24 11:31:29 -0700

    commitA tenn

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -2,47 +2,53 @@ import React from "react";
 import { Dropdown } from 'react-bootstrap';
 import './Appointment.css';
 const Booking = () => {
-    return(
-        <form action="#">
-            <div className="form-group">
-                <Dropdown>
-                    <Dropdown.Toggle variant="light" id="dropdown-basic">
-                        Select Department
-                    </Dropdown.Toggle>
+    return (
+        <section className="section_book">
+            <div className="container">
+                <form action="#">
+                    <div className="form-group">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Select Department
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">One</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Two</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Three</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
+                            <Dropdown.Menu>
+                                <Dropdown.Item className="active" href="#/action-1">Selecty Department</Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">One</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Two</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Three</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
 
-            {/* Select Doctor Dropdown */}
-            <div className="form-group">
-                <Dropdown>
-                    <Dropdown.Toggle variant="light" id="dropdown-basic">
-                        Select Doctor
-                    </Dropdown.Toggle>
+                    {/* Select Doctor Dropdown */}
+                    <div className="form-group">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-basic">
+                                Select Doctor
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">One</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Two</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Three</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                            <Dropdown.Menu>
+                                <Dropdown.Item className="active" href="#/action-1">Selecty Department</Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">One</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Two</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Three</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Your Name" />
+                    </div>
+                    <div className="form-group">
+                        <input type="number" className="form-control" placeholder="Phone Numbers" />
+                    </div>
+                    <div className="form-group">
+                        <input type="date" className="form-control control_date" />
+                    </div>
+                    <button type="submit" className="btn btn-secondary btn_secondary btn-lg">Appointment Now</button>
+                </form>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Name" />
-            </div>
-            <div class="form-group">
-                <input type="number" class="form-control" placeholder="Phone Numbers" />
-            </div>
-            <div class="form-group">
-                <input type="date" class="form-control" />
-            </div>
-            <button type="submit" class="btn btn-secondary btn-lg">Appointment Now</button>
-        </form>
+        </section>
     );
 }
 
