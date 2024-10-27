8d982be0ccb3796d5caaec7a458d2a988b6a5149
Omar <omaralkhuly18@gmail.com>
2024-10-27 12:35:20 -0700

    Update Banner.js
    إضافة import and fun تكون في ال banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { useLocation } from 'react-router-dom'; // استيراد useLocation
 import './Banner.css';
+
 const Banner = () => {
+    const location = useLocation();
+    
+    // حدد اسم الصفحة بناءً على المسار الحالي
+    const getPageName = () => {
+        switch (location.pathname) {
+            case '/About':
+                return 'About Us';
+            case '/Team':
+                return 'Our Team';
+            case '/FAQ':
+                return "FAQ's";
+            case '/Reservation':
+                return 'Booking';
+            case '/Error':
+                return 'Error 404';
+            case '/sign':
+                return 'Login / Register';
+            case '/Services':
+                return 'Services';
+            case '/ServicesDetails':
+                return 'Services Details';
+            case '/Blog':
+                return 'Blog';
+            case '/BlogDetails':
+                return 'Blog Details';
+            case '/Contact':
+                return 'Contact Us';
+            default:
+                return '';
+        }
+    };
+
     return (
         <div className="container">
             <div className="page-banner-entry text-center page_banner_entry">
@@ -12,22 +46,17 @@ const Banner = () => {
                         <li className="breadcrumb_item">
                             <a href="/Home">
                                 <FontAwesomeIcon className="banner_icon" icon={faHome} />
-                                <span>
-                                    Home
-                                </span>
+                                <span>Home</span>
                             </a>
                         </li>
                         <li className="breadcrumb_item active" aria-current="page">
-                            <span>
-                                Contact Us
-                            </span>
+                            <span>{getPageName()}</span> {/* إظهار اسم الصفحة */}
                         </li>
                     </ul>
                 </nav>
             </div>
         </div>
-
     );
 }
 
-export default Banner;
+export default Banner;
