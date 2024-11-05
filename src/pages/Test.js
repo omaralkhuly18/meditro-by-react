985cd9ae67f25ceef3a2aa87d3f50eaa2bd852c2
omarelkhuly <silvertriangle11@gmail.com>
2024-11-05 10:29:06 -0800

    commitC three

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -3,94 +3,37 @@ import React from 'react';
 const TestJs = () => {
     return (
         <>
-            <section className='section-area section-sp1 service-wraper'>
-                <div className="row align-items-center">
-                    <div className="col-xl-4 col-lg-7 mb-30">
-                        <div className="heading-bx">
-                            <h6 className="title-ext text-secondary">Services</h6>
-                            <h2 className="title">We Cover A Big Variety Of Medical Services</h2>
-                            <p>We provide the special tips and advice’s of heath care treatment and high level of best.</p>
-                        </div>
-                        <a href="services.html" className="btn btn-secondary btn-lg shadow">All Services</a>
-                    </div>
-                    <div className="col-xl-8 mb-15">
-                        <div className="swiper-container service-slide swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events">
-                            <div className="swiper-wrapper" id="swiper-wrapper-18d6ad1cbbe0103ee" aria-live="polite">
-                                <div className="swiper-slide swiper-slide-active" role="group" aria-label="1 / 5" style={{ width: "332px" }}>
-                                    <div className="feature-container feature-bx2 feature1">
-                                        <div className="feature-box-xl mb-30">
-                                            <span className="icon-cell">
+        {/* <!doctype html>
+<html lang="en">
 
-                                            </span>
-                                        </div>
-                                        <div className="icon-content">
-                                            <h3 className="ttr-title">Diagnostics</h3>
-                                            <p>Phasellus venenatis porta rhoncus. Integer et viverra felis.</p>
-                                            <a href="service-detail.html" className="btn btn-primary light">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide swiper-slide-next" role="group" aria-label="2 / 5" style={{ width: "332px" }}>
-                                    <div className="feature-container feature-bx2 feature2">
-                                        <div className="feature-box-xl mb-20">
-                                            <span className="icon-cell">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/meditro-by-react/favicon.ico" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Web site created using create-react-app" />
+    <link rel="apple-touch-icon" href="/meditro-by-react/logo192.png" />
+    <link rel="manifest" href="/meditro-by-react/manifest.json" />
+    <title>React App</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/fontawesome.min.css"
+        integrity="sha512-B46MVOJpI6RBsdcU307elYeStF2JKT87SsHZfRSkjVi4/iZ3912zXi45X5/CBr/GbCyLx6M1GQtTKYRd52Jxgw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script defer="defer" src="/meditro-by-react/static/js/main.34a0c861.js"></script>
+    <link href="/meditro-by-react/static/css/main.42da42e0.css" rel="stylesheet">
+</head>
 
-                                            </span>
-                                        </div>
-                                        <div className="icon-content">
-                                            <h3 className="ttr-title">Treatment</h3>
-                                            <p>Phasellus venenatis porta rhoncus. Integer et viverra felis.</p>
-                                            <a href="service-detail.html" className="btn btn-primary light">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide" role="group" aria-label="3 / 5" style={{ width: "332px" }}>
-                                    <div className="feature-container feature-bx2 feature3">
-                                        <div className="feature-box-xl mb-20">
-                                            <span className="icon-cell">
+<body><noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/fontawesome.min.js"
+        integrity="sha512-NeFv3hB6XGV+0y96NVxoWIkhrs1eC3KXBJ9OJiTFktvbzJ/0Kk7Rmm9hJ2/c2wJjy6wG0a0lIgehHjCTDLRwWw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
 
-                                            </span>
-                                        </div>
-                                        <div className="icon-content">
-                                            <h3 className="ttr-title">Surgery</h3>
-                                            <p>Phasellus venenatis porta rhoncus. Integer et viverra felis.</p>
-                                            <a href="service-detail.html" className="btn btn-primary light">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide" role="group" aria-label="4 / 5" style={{ width: "332px" }}>
-                                    <div className="feature-container feature-bx2 feature1">
-                                        <div className="feature-box-xl mb-20">
-                                            <span className="icon-cell">
-                                            </span>
-                                        </div>
-                                        <div className="icon-content">
-                                            <h3 className="ttr-title">Vaccine</h3>
-                                            <p>Phasellus venenatis porta rhoncus. Integer et viverra felis.</p>
-                                            <a href="service-detail.html" className="btn btn-primary light">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="swiper-slide" role="group" aria-label="5 / 5" style={{ width: "332px" }}>
-                                    <div className="feature-container feature-bx2 feature2">
-                                        <div className="feature-box-xl mb-20">
-                                            <span className="icon-cell">
-                                            </span>
-                                        </div>
-                                        <div className="icon-content">
-                                            <h3 className="ttr-title">Emergency</h3>
-                                            <p>Phasellus venenatis porta rhoncus. Integer et viverra felis.</p>
-                                            <a href="service-detail.html" className="btn btn-primary light">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <span className="swiper-notification" aria-live="assertive" aria-atomic="true">
-                                </span>
-                                </div>
-                    </div>
-                </div>
-            </section>
+</html> */}
         </>
     );
 }
